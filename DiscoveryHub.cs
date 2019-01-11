e4649117fc46963c726beb5b22c24e9c20175437
neuroglyph <38789497+neuroglyph@users.noreply.github.com>
2019-01-11 16:45:05 -0500

    Add files via upload

---
+++ DiscoveryHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace eDiscovery
+{
+    public class DiscoveryHub : Hub
+    {
+        public void Send(string name, string resultsCount)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.broadcastMessage(name, resultsCount);
+        }
+
+    }
+}
